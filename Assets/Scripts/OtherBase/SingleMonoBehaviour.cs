573c9642e9d2f4b82601b3f5e6869273e02c2a7b
a995049470 <17855834269@163.com>
2019-07-14 11:09:08 +0800

    7.12

---
+++ Assets/Scripts/OtherBase/SingleMonoBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleMonoBehaviour<T> : MonoBehaviour 
+{
+    private static T m_Instance;
+    private static GameObject m_Go;
+    public static T Instance
+    {
+        get
+        {
+            if(m_Instance == null)
+            {
+                if(m_Go == null)
+                {
+                    m_Go = new GameObject();
+                    m_Go.name = "SingleMonoBehaviour";
+
+                }
+            }
+            return m_Instance;
+        }
+    }
+	
+}
---
+++ Assets/Scripts/WindowBase/Window.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lin.UI
+{
+    public class Window : MonoBehaviour
+    {
+        private bool m_bIsInit = false;
+        protected virtual void BindController()
+        {
+
+        }
+
+        protected virtual void AddListeners()
+        {
+
+        }
+
+        protected virtual void OnPerOpen()
+        {
+
+        }
+
+        protected virtual void OnAfterOpen()
+        {
+
+        }
+
+        protected virtual void OnPerColse()
+        {
+
+        }
+
+        protected virtual void OnAfterClose()
+        {
+
+        }
+
+        public void Open()
+        {
+            try
+            {
+                if (!m_bIsInit)
+                {
+                    BindController();
+                    AddListeners();
+                }
+                OnPerOpen();
+                this.gameObject.SetActive(true);
+                OnAfterOpen();
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+        public void Close()
+        {
+            try
+            {
+                OnPerColse();
+                this.gameObject.SetActive(false);
+                OnAfterClose();
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+
+}
